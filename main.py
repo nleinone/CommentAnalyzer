27d49903a7b4cbdfbc581fc0ceabf8a5c58595fe
nleinone <nikoleino91@gmail.com>
2019-11-13 10:08:00 +0200

    Added cross validation. Added Bigram inspection. Accruacy NB: 78%

--- main.py
+++ main.py
@@ -16,27 +16,11 @@ if __name__ == '__main__':
     '''*** INITIAL CONFIGURATIONS ***'''
     print("Configuring classifiers...")
     
-    #training_data, test_data = generate_classifier_data_sets.get_training_and_test_data(fold)
     training_and_test_data = generate_classifier_data_sets.get_training_and_test_data(cross_validations_fold_ratio)
+    nb_classifier, nb_avg_accuracy = configure_classifiers.configure_all(training_and_test_data)
     
-    nb_classifier, nb_avg_accuracy, dt_classifier, dt_classifier_accuracy = configure_classifiers.configure_all(training_and_test_data)
-    
-    #nb_classifier, nb_classifier_accuracy, dt_classifier, dt_classifier_accuracy = configure_classifiers.configure_all(training_data, test_data)
-    
-    print("1. Naive Bayes (Accuracy {})".format(avg_accuracy * 100))
-    print("2. Decision Tree (Accuracy {})".format(dt_classifier_accuracy * 100))
-    
-    choice = input('Choose classifier: ')
-    
-    if choice == 1:
-        classifier = nb_classifier
-        classifier_accuracy = avg_accuracy
-    elif choice == 2:
-        classifier = dt_classifier
-        classifier_accuracy = dt_classifier_accuracy
-    else:
-        classifier = nb_classifier
-        classifier_accuracy = avg_accuracy
+    classifier = nb_classifier
+    classifier_accuracy = nb_avg_accuracy
         
     '''*** INITIAL CONFIGURATIONS ENDS***'''
     
@@ -45,7 +29,6 @@ if __name__ == '__main__':
     csv_file_names = parsing_functions.fetch_document_names()
     
     for file_name in csv_file_names:
-        
         #Variables
         information_collection = []
         bot_mood_happy_collection = []
@@ -61,18 +44,16 @@ if __name__ == '__main__':
         for file_line in file_lines_list:
             information_dictionary = parsing_functions.distinguish_information(file_line)
             information_collection.append(information_dictionary)
-        #print('information_collection: {}\n'.format(information_collection))
-        
+       
         happy_bot_comments, happysad_bot_comments, sad_bot_comments, neutral_bot_comments, hello_bot_comments = parsing_functions.separate_comments_by_bot_mood(information_collection)
         all_comments = [happy_bot_comments, happysad_bot_comments, sad_bot_comments, neutral_bot_comments, hello_bot_comments]
-        #print(all_comments)
         
-        for comment_set in all_comments:
-            
+        for comment_set in all_comments:         
             try:
                 first_comment = comment_set[0]
                 mood = first_comment['bot_mood']
                 print('Processing {} bot answer data...'.format(mood))
+            
             except:
                 continue
         
@@ -84,52 +65,26 @@ if __name__ == '__main__':
             for comment in comment_set:
                 user_answer = comment['user_answer']
                 
-                #print("user_answer: {}".format(user_answer))
-                
                 #Tokenize comment
                 tokenized_comment = preprocessor.tokenize_comment(user_answer)
-                
-                #print("tokenized_comment: {}".format(tokenized_comment))
-                
+        
                 #Filter stopwords
                 training_mode = False
-                stopword_filtered_comment = preprocessor.filter_stopwords(tokenized_comment, training_mode)
-                
-                #print("stopword_filtered_comment: {}".format(stopword_filtered_comment))
+                bigram = True
+                stopword_filtered_comment = preprocessor.filter_stopwords(tokenized_comment, training_mode, bigram)
                 
                 #Clean the user answer
                 normalized_comment = preprocessor.normalize_and_clean_comment(stopword_filtered_comment)
                 
-                #print("Normalized comment: {}".format(normalized_comment))
-                
                 '''*** NATURAL LANGUAGE PREPROCESSING STOPS HERE***'''
                 
                 '''*** SENTIMENT ANALYSIS WITH NAIVE BAYES STARTS HERE***'''
-
-                normalized_comment_feature_set = generate_classifier_data_sets.remove_duplicates(normalized_comment, training_mode)
-                
-                #print("normalized_comment_feature_set: {}".format(normalized_comment_feature_set))
-                
+            
+                normalized_comment_feature_set = generate_classifier_data_sets.extract_features(normalized_comment, normalized_comment, training_mode)
                 probability_result = classifier.prob_classify(normalized_comment_feature_set)
                 
                 #Print statistics:
                 generate_classifier_data_sets.print_statistics(probability_result, classifier, normalized_comment_feature_set, user_answer, classifier_accuracy)
-                #naive_bayes_classifier.find_word_from_training_set(normalized_comment, normalized_comment_features)
-                
     #Runtime stamp
-    
-    
     print("Runtime: {}\n".format(datetime.now() - datetime_start))
                 
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-    
--- preprocessing/preprocessor.py
+++ preprocessing/preprocessor.py
@@ -1,6 +1,7 @@
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 
 def normalize_and_clean_comment(stopword_filtered_comment):
     """Make all words lowercase and clean words which are not alphabetic"""
@@ -14,14 +15,18 @@ def normalize_and_clean_comment(stopword_filtered_comment):
                 single_comment.append(word.lower())
         if len(single_comment) != 0:
             cleaned_comment.append(single_comment)
-    #print('cleaned: {}'.format(cleaned_comment))
     
     return cleaned_comment
     
-def filter_stopwords(tokenized_answer, training_mode):
+def filter_stopwords(tokenized_answer, training_mode, bigram):
     '''Filter stopwords'''
     
+    not_filtered = ['above', 'below', 'off', 'over', 'under', 'more', 'most', 'such', 'no', 'nor', 'not', 'only', 'so', 'than', 'too', 'very', 'just', 'but']
+ 
     stop_words = set(stopwords.words('english'))
+    
+    if bigram:
+        stop_words = set(stopwords.words('english')) - set(not_filtered)   
     
     stopword_filtered_answer = []
 
@@ -33,9 +38,6 @@ def filter_stopwords(tokenized_answer, training_mode):
                     single_comment.append(word)
             if len(single_comment) != 0:
                 stopword_filtered_answer.append(single_comment)
-    
-        #print('filtered: {}'.format(stopword_filtered_answer))
-        
     
     else:
         single_comment = []
@@ -51,5 +53,31 @@ def tokenize_comment(user_answer):
     '''Tokenize user answer'''
     
     tokenized_answer = nltk.word_tokenize(user_answer)
-    #print('tokenized: {}'.format(tokenized_answer))
     return tokenized_answer
+    
+def stem_sentence(sentences, training_mode):
+    '''Remove suffixes (and other end components) from the words, returning only the root part of the word'''
+    stemmed_sentences = []
+    stemmer = PorterStemmer() 
+        
+    if training_mode:
+        for comment in sentences:
+            single_comment = []
+            for word in comment:
+                
+                stemmed_word = stemmer.stem(word)
+                single_comment.append(stemmed_word)
+                
+            if len(single_comment) != 0:
+                stemmed_sentences.append(single_comment)
+    
+    else:
+        single_comment = []
+        for word in sentences:
+            stemmed_word = PorterStemmer.stem(word)
+            single_comment.append(stemmed_word)
+            
+        if len(single_comment) != 0:
+            stemmed_sentences.append(single_comment)
+
+    return stemmed_sentences
